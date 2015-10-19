6966e950e85795bb4f1bee3b95c80fe2863c7380
Michael Bestas <mikeioannina@gmail.com>
2015-10-19 21:57:47 +0300

    Add LX versioning

---
+++ config/common.mk
@@ -0,0 +1,13 @@
+PRODUCT_VERSION_MAJOR = 6
+PRODUCT_VERSION_MINOR = 0
+
+# Filter out random types, so it'll reset to UNOFFICIAL
+ifeq ($(filter NIGHTLY SNAPSHOT,$(LX_BUILDTYPE)),)
+    LX_BUILDTYPE := UNOFFICIAL
+endif
+
+LX_VERSION := $(PRODUCT_VERSION_MAJOR).$(PRODUCT_VERSION_MINOR)-$(shell date -u +%Y%m%d)-$(LX_BUILDTYPE)-$(LX_BUILD)
+
+PRODUCT_PROPERTY_OVERRIDES += \
+  ro.lx.version=$(LX_VERSION) \
+  ro.modversion=$(LX_VERSION)
